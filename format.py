cc094c4ef8dee6a4e82f93e868fef6df5b3c9bd0
John Maximilian <2e0byo@gmail.com>
2022-01-06 14:12:42 +0000

    feat(format.py): handle multiple files at once.

--- format.py
+++ format.py
@@ -24,7 +24,7 @@ def load_uniq(fn):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("FN", help="File to format.", type=Path)
+    parser.add_argument("FNS", help="File to format.", type=Path, nargs="+")
     parser.add_argument(
         "--verify",
         help="Exit with status 0 if nothing to be done else 1.",
@@ -32,25 +32,27 @@ def main():
     )
 
     args = parser.parse_args()
-    fn = args.FN.expanduser()
 
-    print(f"Loading and uniqifying {fn}")
-    db = load_uniq(fn)
-    writer = BibTexWriter()
-    writer.order_entries_by = ("author", "year")
-    writer.comma_first = True
-    generated = StringIO(writer.write(db))
-    if not args.verify:
-        with fn.open("w") as f:
-            f.write(generated.read())
-        print(f"Saved {fn}")
-    else:
-        with fn.open() as f:
-            for left, right in zip(f, generated):
-                if left != right:
-                    stderr.write(f"{fn} failed verification")
-                    stderr.flush()
-                    exit(1)
+    for fn in args.FNS:
+        fn = fn.expanduser()
+
+        print(f"Loading and uniqifying {fn}")
+        db = load_uniq(fn)
+        writer = BibTexWriter()
+        writer.order_entries_by = ("author", "year")
+        writer.comma_first = True
+        generated = StringIO(writer.write(db))
+        if not args.verify:
+            with fn.open("w") as f:
+                f.write(generated.read())
+            print(f"Saved {fn}")
+        else:
+            with fn.open() as f:
+                for left, right in zip(f, generated):
+                    if left != right:
+                        stderr.write(f"{fn} failed verification")
+                        stderr.flush()
+                        exit(1)
 
 
 if __name__ == "__main__":
